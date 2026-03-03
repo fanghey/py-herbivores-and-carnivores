718878c82d040e0e03b4decbe0efcea2caca28fc
fanghey <2603Lysenko2603@gmail.com>
2026-03-03 14:19:27 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 class Animal:
     alive = []
 
-    def __init__(self, name: str, health: int = 100, hidden: bool = False):
+    def __init__(self, name: str, health: int = 100, hidden: bool = False) -> None:
         self.name = name
         self.health = health
         self.hidden = hidden
@@ -17,7 +17,7 @@ class Animal:
 
 
 class Herbivore(Animal):
-    def hide(self):
+    def hide(self) -> None:
         self.hidden = not self.hidden
 
 
